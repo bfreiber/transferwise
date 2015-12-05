1f6c73e7cc7b2e4cb00fea4885f547fd8697ce35
Brandon Freiberg <freiberg.brandon@gmail.com>
2015-12-05 01:52:14 -0500

    first commit!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn transferwise.wsgi --log-file -
---
+++ transferwise/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for transferwise project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transferwise.settings")
+
+application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
